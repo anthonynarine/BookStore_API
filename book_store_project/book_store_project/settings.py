5547aab5542517ceabf61b69ccebb64f37fcaf88
anthonynarine <anarine83@gmail.com>
2022-10-16 13:05:46 -0400

    Books App Create + Book Model created + Migrated

--- book_store_project/book_store_project/settings.py
+++ book_store_project/book_store_project/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "books",
+    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
---
+++ book_store_project/books/models.py
@@ -0,0 +1,13 @@
+from ast import Mod
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    quantity = models.IntegerField()
+    
+    
